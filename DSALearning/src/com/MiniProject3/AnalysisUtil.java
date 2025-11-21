538b4f97c07a565f4e89cc3a3d06f881a4a251b7
FatemaM-27 <fatemavadgam5180>
2025-11-22 00:45:34 +0530

    feat:added mini project 3 file

--- DSALearning/src/com/MiniProject3/AnalysisUtil.java
+++ DSALearning/src/com/MiniProject3/AnalysisUtil.java
@@ -1,4 +1,10 @@
 package com.MiniProject3;
 
 public class AnalysisUtil {
+    public static long measureTime(Runnable algorithm) {
+        long start = System.nanoTime();
+        algorithm.run();
+        return System.nanoTime() - start;
+    }
 }
+
